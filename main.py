c7fe5c1bb8e92d21b038c61c975a3d608b06aa47
hirochanpu <scashi45gare11@gmail.com>
2026-06-12 13:12:45 +0900

    Update image path from '図12.png' to '図13.png'

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def timer(label):
 
 # --- 1. 準備：住所（パス）の整理 ---
 base_dir = os.path.dirname(__file__)
-img_path = os.path.join(base_dir, "図12.png") 
+img_path = os.path.join(base_dir, "図13.png") 
 excel_path = os.path.join(base_dir, "ONE_PIECE_DATE_FILE_ver.13.xlsx")
 
 st.set_page_config(page_title="ONE PIECE 航海記", layout="wide")
